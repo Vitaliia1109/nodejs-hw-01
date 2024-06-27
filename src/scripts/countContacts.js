e37703c07942b2323c8a296de02f027be23dbf07
Vitaliia1109 <vitusechka@ukr.net>
2024-06-27 08:13:02 +0100

    try-3

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -5,13 +5,10 @@ export const countContacts = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
-    // console.log(`Number of contacts: ${contacts.length}`);
     return `Number of contacts: ${contacts.length}`;
   } catch (error) {
     console.error('Error counting contacts:', error);
   }
 };
-
-// countContacts();
 
 console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -12,6 +12,4 @@ export const getAllContacts = async () => {
   }
 };
 
-// getAllContacts();
-
 console.log(await getAllContacts());
